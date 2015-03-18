45af8ae9bf73dfa6e16420fb8be38405cf2e2bc0
Danny Roberts <danny.roberts@reconnix.com>
2015-03-18 09:13:29 +0000

    Fixed variable names

--- mysql/mysql_dump.sh
+++ mysql/mysql_dump.sh
@@ -11,9 +11,9 @@ databases=($(mysql -BN -e "show databases"))
 
 checkexistingbackups () {
   if $COMPRESS; then
-    oldbackup="$BACKUPDIR/$db-$(date --date="$RETENTION days ago" +"%Y%m%d").sql.gz"
+    oldbackup="$BACKUPDIR/$1-$(date --date="$RETENTION days ago" +"%Y%m%d").sql.gz"
   else
-    oldbackup="$BACKUPDIR/$db-$(date --date="3 days ago" +"%Y%m%d").sql"
+    oldbackup="$BACKUPDIR/$1-$(date --date="3 days ago" +"%Y%m%d").sql"
   fi
   if [ -e "$oldbackup" ]; then
     rm -f "$oldbackup"
@@ -22,11 +22,11 @@ checkexistingbackups () {
 
 createdbbackup () {
   if $COMPRESS; then
-    mysqldump -h$DBHOST -u$DBUSER -p$DBPASS "$db" | gzip > "$BACKUPDIR/$db-$(date +%Y+%m+%d).sql.gz"
-    mysql -BN -h"$1" -u"$2" -p"$3" -e "SELECT DISTINCT CONCAT('SHOW GRANTS FOR \'', user, '\'@\'', host, '\';') AS query FROM mysql.user" | mysql -h"$1" -u"$2" -p"$3" | sed 's/\(GRANT .*\)/\1;/;s/^\(Grants for .*\)/## \1 ##/;/##/{x;p;x;}' | gzip > "$BACKUPDIR/usergrants-$(date +%Y+%m+%d).sql.gz"
+    mysqldump -h$DBHOST -u$DBUSER -p$DBPASS "$1" | gzip > "$BACKUPDIR/$1-$(date +%Y+%m+%d).sql.gz"
+    mysql -BN -h$DBHOST -u$DBUSER -p$DBPASS -e "SELECT DISTINCT CONCAT('SHOW GRANTS FOR \'', user, '\'@\'', host, '\';') AS query FROM mysql.user" | mysql -h$DBHOST -u$DBUSER -p$DBPASS | sed 's/\(GRANT .*\)/\1;/;s/^\(Grants for .*\)/## \1 ##/;/##/{x;p;x;}' | gzip > "$BACKUPDIR/usergrants-$(date +%Y+%m+%d).sql.gz"
   else
-    mysqldump -h$DBHOST -u$DBUSER -p$DBPASS "$db" > "$BACKUPDIR/$db-$(date +%Y+%m+%d).sql"
-    mysql -BN -h"$1" -u"$2" -p"$3" -e "SELECT DISTINCT CONCAT('SHOW GRANTS FOR \'', user, '\'@\'', host, '\';') AS query FROM mysql.user" | mysql -h"$1" -u"$2" -p"$3" | sed 's/\(GRANT .*\)/\1;/;s/^\(Grants for .*\)/## \1 ##/;/##/{x;p;x;}' > "$BACKUPDIR/usergrants-$(date +%Y+%m+%d).sql"
+    mysqldump -h$DBHOST -u$DBUSER -p$DBPASS "$1" > "$BACKUPDIR/$1-$(date +%Y+%m+%d).sql"
+    mysql -BN -h$DBHOST -u$DBUSER -p$DBPASS -e "SELECT DISTINCT CONCAT('SHOW GRANTS FOR \'', user, '\'@\'', host, '\';') AS query FROM mysql.user" | -h$DBHOST -u$DBUSER -p$DBPASS | sed 's/\(GRANT .*\)/\1;/;s/^\(Grants for .*\)/## \1 ##/;/##/{x;p;x;}' > "$BACKUPDIR/usergrants-$(date +%Y+%m+%d).sql"
   fi
 }
 
