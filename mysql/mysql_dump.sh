e001d40ed7b62af787f8fd1124762c993b2dec65
Danny Roberts <danny.roberts@reconnix.com>
2015-03-18 08:58:37 +0000

    Script now dumps user grants as well so these can be recreated if required

--- mysql/mysql_dump.sh
+++ mysql/mysql_dump.sh
@@ -23,8 +23,10 @@ checkexistingbackups () {
 createdbbackup () {
   if $COMPRESS; then
     mysqldump -h$DBHOST -u$DBUSER -p$DBPASS "$db" | gzip > "$BACKUPDIR/$db-$(date +%Y+%m+%d).sql.gz"
+    mysql -BN -h"$1" -u"$2" -p"$3" -e "SELECT DISTINCT CONCAT('SHOW GRANTS FOR \'', user, '\'@\'', host, '\';') AS query FROM mysql.user" | mysql -h"$1" -u"$2" -p"$3" | sed 's/\(GRANT .*\)/\1;/;s/^\(Grants for .*\)/## \1 ##/;/##/{x;p;x;}' | gzip > "$BACKUPDIR/usergrants-$(date +%Y+%m+%d).sql.gz"
   else
     mysqldump -h$DBHOST -u$DBUSER -p$DBPASS "$db" > "$BACKUPDIR/$db-$(date +%Y+%m+%d).sql"
+    mysql -BN -h"$1" -u"$2" -p"$3" -e "SELECT DISTINCT CONCAT('SHOW GRANTS FOR \'', user, '\'@\'', host, '\';') AS query FROM mysql.user" | mysql -h"$1" -u"$2" -p"$3" | sed 's/\(GRANT .*\)/\1;/;s/^\(Grants for .*\)/## \1 ##/;/##/{x;p;x;}' > "$BACKUPDIR/usergrants-$(date +%Y+%m+%d).sql"
   fi
 }
 
