ee04ddf1204c72e9c15e06f916cf0be21d1a480a
Danny Roberts <danny.roberts@reconnix.com>
2015-03-18 14:56:42 +0000

    Added sensible set of default options

--- mysql/mysql_dump.sh
+++ mysql/mysql_dump.sh
@@ -22,16 +22,18 @@ checkexistingbackups () {
 
 createdbbackup () {
   if $COMPRESS; then
-    mysqldump -h$DBHOST -u$DBUSER -p$DBPASS "$1" | gzip > "$BACKUPDIR/$1-$(date +%Y+%m+%d).sql.gz"
-    mysql -BN -h$DBHOST -u$DBUSER -p$DBPASS -e "SELECT DISTINCT CONCAT('SHOW GRANTS FOR \'', user, '\'@\'', host, '\';') AS query FROM mysql.user" | mysql -h$DBHOST -u$DBUSER -p$DBPASS | sed 's/\(GRANT .*\)/\1;/;s/^\(Grants for .*\)/## \1 ##/;/##/{x;p;x;}' | gzip > "$BACKUPDIR/usergrants-$(date +%Y+%m+%d).sql.gz"
+    mysqldump -h$DBHOST -u$DBUSER -p$DBPASS -cC --add-drop-table --add-locks --quick "$1" | gzip > "$BACKUPDIR/$1-$(date +%Y%m%d).sql.gz"
+    mysql -BN -h$DBHOST -u$DBUSER -p$DBPASS -e "SELECT DISTINCT CONCAT('SHOW GRANTS FOR \'', user, '\'@\'', host, '\';') AS query FROM mysql.user" | mysql -h$DBHOST -u$DBUSER -p$DBPASS | sed 's/\(GRANT .*\)/\1;/;s/^\(Grants for .*\)/## \1 ##/;/##/{x;p;x;}' | gzip > "$BACKUPDIR/usergrants-$(date +%Y%m%d).sql.gz"
   else
-    mysqldump -h$DBHOST -u$DBUSER -p$DBPASS "$1" > "$BACKUPDIR/$1-$(date +%Y+%m+%d).sql"
-    mysql -BN -h$DBHOST -u$DBUSER -p$DBPASS -e "SELECT DISTINCT CONCAT('SHOW GRANTS FOR \'', user, '\'@\'', host, '\';') AS query FROM mysql.user" | -h$DBHOST -u$DBUSER -p$DBPASS | sed 's/\(GRANT .*\)/\1;/;s/^\(Grants for .*\)/## \1 ##/;/##/{x;p;x;}' > "$BACKUPDIR/usergrants-$(date +%Y+%m+%d).sql"
+    mysqldump -h$DBHOST -u$DBUSER -p$DBPASS -cC --add-drop-table --add-locks --quick "$1" > "$BACKUPDIR/$1-$(date +%Y%m%d).sql"
+    mysql -BN -h$DBHOST -u$DBUSER -p$DBPASS -e "SELECT DISTINCT CONCAT('SHOW GRANTS FOR \'', user, '\'@\'', host, '\';') AS query FROM mysql.user" | -h$DBHOST -u$DBUSER -p$DBPASS | sed 's/\(GRANT .*\)/\1;/;s/^\(Grants for .*\)/## \1 ##/;/##/{x;p;x;}' > "$BACKUPDIR/usergrants-$(date +%Y%m%d).sql"
   fi
 }
 
 for db in "${databases[@]}"; do
   if [ "$db" == 'mysql' ]; then
+    continue
+  elif [ "$db" == 'information_schema' ]; then
     continue
   fi
   checkexistingbackups "$db"
